8c51768cf004db97f3568a3dd0279635ec3a3284
6D157 <6d157@kanrishanoiMac.local>
2021-11-04 15:46:38 +0900

    add

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,68 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      書籍名：
+      <input type="text" v-model="searchbox">
+      <button @click="search">検索する</button>
+    </div>
+    <div>
+      著者名：
+      <input type="text" v-model="searchbox2">
+      <button @click="search2">検索する</button>
+      <ul>
+        <li v-for="book in books" :key="book.id">
+          <img :src="book.volumeInfo.imageLinks.smallThumbnail" alt="">
+          {{book.volumeInfo.title}}
+          {{book.volumeInfo.authors[0]}}
+        </li>
+      </ul>      <!-- <ul>
+        <li v-for="author in authors" :key="author.id">
+          <img :src="book.volumeInfo.imageLinks.smallThumbnail" alt="">
+          {{book.volumeInfo.title}}
+
+        </li>
+      </ul> -->
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      books: '',
+      searchbox: '',
+      orderBy: 'newest',
+      maxResults: '10',
+      searchbox2: '',
+      authors: '',
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+    search() {
+      axios.get(
+          `https://www.googleapis.com/books/v1/volumes?q=intitle:${this.searchbox}&orderBy=${this.orderBy}&maxResults=${this.maxResults}`
+        )
+        .then(response => {
+          console.log(response.data.items)
+          this.books = response.data.items 
+        })
+    },
+    search2() {
+      axios.get(
+          `https://www.googleapis.com/books/v1/volumes?q=inauthor:${this.searchbox2}`
+        )
+        .then(response => {
+          console.log(response.data.items)
+          this.books = response.data.items 
+        })
+    }
   }
 }
 </script>
